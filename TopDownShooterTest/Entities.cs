823f6669023e211c6019de68caad6fdad6daa886
Shots243 <fawwazandsyafiq@gmail.com>
2019-03-02 06:18:10 +0000

    initial commit; added incomplete base files

---
+++ TopDownShooterTest/Entities.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TopDownShooterTest
+{
+    class PlayerShip
+    {
+        private Texture2D ShipTexture { get; set; }
+        public int MaxHP { get; set; }
+        public int MaxSE { get; set; }
+        public int ShieldRegenRate { get; set; }
+        public Weapon ShipWeapon { get; set; }
+        public int RateOfFire { get; set; }
+        public int ShipSpeed { get; set; }
+
+        private int CurrentShotCooldownTimer = 0;
+        public Vector2 ShipCoords = new Vector2(0, 0);
+
+        public PlayerShip(Texture2D shipTexture, int maxHealth, int maxShield, int shieldRegenRate, int speed, Weapon givenWeapon)
+        {
+            ShipTexture = shipTexture;
+            ShipWeapon = givenWeapon;
+            MaxHP = MaxHP;
+            MaxSE = maxShield;
+            ShieldRegenRate = shieldRegenRate;
+            ShipSpeed = speed;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            spriteBatch.Draw(ShipTexture, new Vector2(ShipCoords.X - (ShipTexture.Width / 2), ShipCoords.Y - (ShipTexture.Height / 2)), Color.White);
+            spriteBatch.End();
+        }
+
+        public int GiveDiagonalSpeed()
+        {
+            int DSpeed = (int)(Math.Sqrt(2) * (ShipSpeed / 2));
+            return DSpeed;
+        }
+
+        public void ModifyShipPosition(int axis, int amount)
+        {
+            if (axis == 0)
+            {
+                int NewX = 0;
+                if (ShipCoords.X + amount < 0)
+                {
+                    NewX = 0;
+                }
+                else if (ShipCoords.X + amount > 480)
+                {
+                    NewX = 480;
+                }
+                else
+                {
+                    NewX = (int)ShipCoords.X + amount;
+                }
+
+                ShipCoords = new Vector2(NewX, ShipCoords.Y);
+            }
+            else
+            {
+                int NewY = 0;
+                if (ShipCoords.Y + amount < 0)
+                {
+                    NewY = 0;
+                }
+                else if (ShipCoords.Y + amount > 640)
+                {
+                    NewY = 640;
+                }
+                else
+                {
+                    NewY = (int)ShipCoords.Y + amount;
+                }
+
+                ShipCoords = new Vector2(ShipCoords.X, NewY);
+            }
+        }
+    }
+
+    class Projectile
+    {
+        private Texture2D ProjectileTexture { get; set; }
+        public int ProjectileDamage { get; set; }
+
+        public Projectile(Texture2D projectileTexture, int damage)
+        {
+            ProjectileTexture = projectileTexture;
+            ProjectileDamage = damage;
+        }
+    }
+
+    class Weapon
+    {
+        public string IdentifierName { get; set; }
+        public string WeaponName { get; set; }
+        public Projectile WeaponProjectile { get; set; }
+        public int RateDelay { get; set; }
+        public int ShotCount { get; set; }
+        public int SpreadType { get; set; }
+        public float SpreadAngle { get; set; }
+
+        private int CurrentDelay = 0;
+
+        public Weapon(string idName, string name, Projectile shotProjectile, int rof, int amount, int spreadType, float spreadAngle)
+        {
+
+        }
+    }
+}
---
+++ TopDownShooterTest/MainGame.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TopDownShooterTest
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class MainGame : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        
+        public MainGame()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        PlayerShip CurrentShip;
+        bool ShipInitialised = false;
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            graphics.PreferredBackBufferWidth = 480;
+            graphics.PreferredBackBufferHeight = 640;
+            graphics.ApplyChanges();
+
+            base.IsMouseVisible = true;
+            base.Initialize();
+        }
+
+        Texture2D PlayerShipTexture;
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            PlayerShipTexture = Content.Load<Texture2D>("Ship");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        Keys UpKey = Keys.Up;
+        Keys DownKey = Keys.Down;
+        Keys LeftKey = Keys.Left;
+        Keys RightKey = Keys.Right;
+        Keys ShootKey = Keys.Space;
+        Keys FocusKey = Keys.LeftShift;
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            KeyboardState CurrentKB = Keyboard.GetState();
+
+            if (!ShipInitialised)
+            {
+                CurrentShip = new PlayerShip(PlayerShipTexture, 100, 100, 10, 6, null);
+                CurrentShip.ShipCoords = new Vector2(240, 540);
+                ShipInitialised = true;
+            }
+
+            if (CurrentKB.IsKeyDown(LeftKey) && CurrentKB.IsKeyDown(UpKey) && CurrentKB.IsKeyUp(DownKey) && CurrentKB.IsKeyUp(RightKey))  // UL
+            {
+                CurrentShip.ModifyShipPosition(0, -(CurrentShip.GiveDiagonalSpeed() / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+                CurrentShip.ModifyShipPosition(1, -(CurrentShip.GiveDiagonalSpeed() / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+            }
+            else if (CurrentKB.IsKeyUp(LeftKey) && CurrentKB.IsKeyDown(UpKey) && CurrentKB.IsKeyUp(DownKey) && CurrentKB.IsKeyDown(RightKey))  // UR
+            {
+                CurrentShip.ModifyShipPosition(0, (CurrentShip.GiveDiagonalSpeed() / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+                CurrentShip.ModifyShipPosition(1, -(CurrentShip.GiveDiagonalSpeed() / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+            }
+            else if (CurrentKB.IsKeyDown(LeftKey) && CurrentKB.IsKeyUp(UpKey) && CurrentKB.IsKeyDown(DownKey) && CurrentKB.IsKeyUp(RightKey))  // DL
+            {
+                CurrentShip.ModifyShipPosition(0, -(CurrentShip.GiveDiagonalSpeed() / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+                CurrentShip.ModifyShipPosition(1, (CurrentShip.GiveDiagonalSpeed() / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+            }
+            else if (CurrentKB.IsKeyUp(LeftKey) && CurrentKB.IsKeyUp(UpKey) && CurrentKB.IsKeyDown(DownKey) && CurrentKB.IsKeyDown(RightKey))  // DR
+            {
+                CurrentShip.ModifyShipPosition(0, (CurrentShip.GiveDiagonalSpeed() / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+                CurrentShip.ModifyShipPosition(1, (CurrentShip.GiveDiagonalSpeed() / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+            }
+            else if (CurrentKB.IsKeyDown(LeftKey) && CurrentKB.IsKeyUp(RightKey))  // L
+            {
+                CurrentShip.ModifyShipPosition(0, -(CurrentShip.ShipSpeed / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+            }
+            else if (CurrentKB.IsKeyUp(LeftKey) && CurrentKB.IsKeyDown(RightKey))  // R
+            {
+                CurrentShip.ModifyShipPosition(0, (CurrentShip.ShipSpeed / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+            }
+            else if (CurrentKB.IsKeyDown(UpKey) && CurrentKB.IsKeyUp(DownKey))  // U
+            {
+                CurrentShip.ModifyShipPosition(1, -(CurrentShip.ShipSpeed / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+            }
+            else if (CurrentKB.IsKeyUp(UpKey) && CurrentKB.IsKeyDown(DownKey))  // D
+            {
+                CurrentShip.ModifyShipPosition(1, (CurrentShip.ShipSpeed / (CurrentKB.IsKeyDown(FocusKey) ? 2 : 1)));
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            CurrentShip.Draw(spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
